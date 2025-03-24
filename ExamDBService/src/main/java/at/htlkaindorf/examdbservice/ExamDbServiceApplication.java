0ee66bb29aad597cf58cc049097026f6e77bd657
haicha21 <haicha21@htl-kaindorf.at>
2025-03-24 09:51:33 +0100

    add project info

---
+++ ExamDBService/src/main/java/at/htlkaindorf/examdbservice/ExamDbServiceApplication.java
@@ -0,0 +1,13 @@
+package at.htlkaindorf.examdbservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamDbServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamDbServiceApplication.class, args);
+    }
+
+}
---
+++ ExamDBService/src/test/java/at/htlkaindorf/examdbservice/ExamDbServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package at.htlkaindorf.examdbservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamDbServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
