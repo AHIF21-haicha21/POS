515318770f3051226bce727e16d803e1e7fd1c12
haicha21 <haicha21@htl-kaindorf.at>
2025-03-31 12:35:59 +0200

    pos theorie

--- SpringExtended/src/main/java/at/htlkaindorf/springextended/controller/BookController.java
+++ SpringExtended/src/main/java/at/htlkaindorf/springextended/controller/BookController.java
@@ -3,6 +3,7 @@ package at.htlkaindorf.springextended.controller;
 import at.htlkaindorf.springextended.dto.BookDTO;
 import at.htlkaindorf.springextended.service.BookService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,5 +18,12 @@ public class BookController {
     @GetMapping
     public ResponseEntity<Iterable<BookDTO>> getAllBooks() {
         return ResponseEntity.ok(bookService.findAllBooks());
+    }
+
+    @GetMapping("/paged")
+    private ResponseEntity<Iterable<BookDTO>> getAllBooksPage(
+            Pageable pageable
+    ) {
+        return ResponseEntity.ok(bookService.findAllBooks(pageable));
     }
 }
--- SpringExtended/src/main/java/at/htlkaindorf/springextended/service/BookService.java
+++ SpringExtended/src/main/java/at/htlkaindorf/springextended/service/BookService.java
@@ -5,6 +5,10 @@ import at.htlkaindorf.springextended.mapper.BookMapper;
 import at.htlkaindorf.springextended.pojos.Book;
 import at.htlkaindorf.springextended.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -22,5 +26,12 @@ public class BookService {
         return books.stream()
                 .map(bookMapper::toDTO)
                 .toList();
+    }
+
+    public List<BookDTO> findAllBooks(Pageable pageable) {
+//        Pageable pageable1 = PageRequest.of(0, 5, Sort.by("title"));
+
+        Page<Book> books = bookRepository.findAll(pageable);
+        return books.map(bookMapper::toDTO).stream().toList();
     }
 }
